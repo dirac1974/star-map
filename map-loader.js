cc42570f6c21dd93527854d4a52513d2266af7d1
dirac1974 <davidstucke@gmail.com>
2026-08-24 16:08:32 -0700

    Use CORS-enabled gist URL for state outlines

--- map-loader.js
+++ map-loader.js
@@ -1,5 +1,5 @@
 window.MAP_PATHS = window.MAP_PATHS || {};
-window.__mapReady = fetch("https://cdn.jsdelivr.net/gh/NPashaP/a74faf20b492ad377312/uStates.js")
+window.__mapReady = fetch("https://gist.githubusercontent.com/NPashaP/a74faf20b492ad377312/raw/uStates.js")
   .then(function (r) { return r.text(); })
   .then(function (txt) {
     var re = /\{id:"([A-Z]{2})",n:"[^"]*",d:"([^"]+)"\}/g;
@@ -9,6 +9,6 @@ window.__mapReady = fetch("https://cdn.jsdelivr.net/gh/NPashaP/a74faf20b492ad377
       window.MAP_PATHS[id] = m[2];
     }
   })
-  .catch(function () {
-    console.warn("Map outlines did not load from the CDN.");
+  .catch(function (err) {
+    console.warn("Map outlines did not load.", err);
   });
